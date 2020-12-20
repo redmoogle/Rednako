deba508bd131af9b7edfa9ac693d760361333d84
redmoogle <dakotamew@gmail.com>
2020-12-20 10:23:19 -0500

    test push

--- commands/admin.py
+++ commands/admin.py
@@ -38,7 +38,7 @@ class Admin(commands.Cog):
     )
     @commands.is_owner()
     async def update(self, ctx):
-        print(f'Local: {self.local}, Remote: {self.remote}')
+        print(f'Local Commit: {self.local}, Remote: {self.remote}')
         if(self.local != self.remote):
             info = [
                     ['Local Commit: ',  f'{self.local}'],
