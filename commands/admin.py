1490fdb65a902ce55a4a88196d18fc9bea6f3ebf
redmoogle <dakotamew@gmail.com>
2021-01-16 16:15:24 -0500

    need better testing

--- commands/admin.py
+++ commands/admin.py
@@ -23,9 +23,9 @@ async def grabmute(ctx, victim: discord.Member = None):
     if victim is None:
         return False
     muteparams = pointer.execute(f'SELECT * FROM mutes WHERE id = {int(victim.id)} AND guild = {int(victim.guild.id)}')
-    if not muteparams.fetchone():
+    if muteparams.fetchone() is None:
         muteparams = pointer.execute(f'SELECT * FROM longmutes WHERE id = {int(victim.id)} AND guild = {int(victim.guild.id)}')
-    if not muteparams.fetchone():
+    if muteparams.fetchone() is None:
         await ctx.send('They were never muted. If this wasnt supposed to be contact a coder', delete_after=3)
         return False
     return muteparams
