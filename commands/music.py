11f52aecb4057c317717f36f6924515f177bce02
redmoogle <dakotamew@gmail.com>
2021-01-31 12:58:19 -0500

    fix

--- commands/music.py
+++ commands/music.py
@@ -130,8 +130,7 @@ class Music(commands.Cog):
         else:
             return await ctx.send('You\'re not in a voice channel')
         player = await lavalink.connect(destination)
-        track = await player.search_yt(search_terms)
-        print(track.tracks)
+        track = await (player.search_yt(search_terms)).tracks[0]
         player.add(ctx.author, track)
 
         embed = discord.Embed(color=discord.Color.blurple())
