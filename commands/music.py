e5b0711c15f62b34461406e385c24df9a9c63a90
redmoogle <dakotamew@gmail.com>
2021-01-31 14:53:44 -0500

    python fuckery

--- commands/music.py
+++ commands/music.py
@@ -40,13 +40,13 @@ def parse_duration(duration: int):
 
         duration = []
         if days > 0:
-            duration.append('{}:'.format(days))
+            duration.append('{}:'.format(int(days)))
         if hours > 0:
-            duration.append('{}:'.format(hours))
+            duration.append('{}:'.format(int(hours)))
         if minutes > 0:
-            duration.append('{}:'.format(minutes))
+            duration.append('{}:'.format(int(minutes)))
         if seconds > 0:
-            duration.append('{}:'.format(seconds))
+            duration.append('{}:'.format(int(seconds)))
 
         return ''.join(duration)
 
