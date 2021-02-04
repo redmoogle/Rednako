03093e997c147ee28e7cd3119954f6aaff1fbfd3
redmoogle <dakotamew@gmail.com>
2021-02-03 21:02:58 -0500

    Update music.py

--- commands/music.py
+++ commands/music.py
@@ -335,11 +335,10 @@ class Music(commands.Cog):
                 await ctx.send('*⃣ | Bot is not playing any music.')
 
         gain = max(min(1, gain), -0.25)
-        player.set_gains((0, gain*.75),(1, gain*.75),(2, gain*.75),(3, gain),(4, gain*.75))
-        if gain > 0:
-            await ctx.send(f'Bass set to {(gain-1)*100}%')
-        elif gain < 0 :
-            await ctx.send(f'Bass set to {(1-gain)*100}%')
+        death = [(0, gain*.75),(1, gain*.75),(2, gain*.75),(3, gain),(4, gain*.75)]
+        player.set_gains(death)
+        if gain:
+            await ctx.send(f'Bass set to {gain*100}%')
         else:
             await ctx.send('Bass set to 100%')
 
@@ -356,11 +355,10 @@ class Music(commands.Cog):
                 await ctx.send('*⃣ | Bot is not playing any music.')
 
         gain = max(min(1, gain), -0.25)
-        player.set_gains((5, gain*.75),(6, gain*.75),(7, gain*.75),(8, gain),(9, gain*.75))
-        if gain > 0:
-            await ctx.send(f'Mids set to {(gain-1)*100}%')
-        elif gain < 0:
-            await ctx.send(f'Mids set to {(1-gain)*100}%')
+        death = [(5, gain*.75),(6, gain*.75),(7, gain*.75),(8, gain),(9, gain*.75)]
+        player.set_gains(death)
+        if gain:
+            await ctx.send(f'Mids set to {gain*100}%')
         else:
             await ctx.send('Mids set to 100%')
 
@@ -376,14 +374,11 @@ class Music(commands.Cog):
             if player.current is None:
                 await ctx.send('*⃣ | Bot is not playing any music.')
 
-        await ctx.send(f'Unadj Gain {gain}')
         gain = max(-0.25, min(1, gain))
-        await ctx.send(f'adj Gain {gain}')
-        player.set_gains((10, gain*.75),(11, gain*.75),(12, gain*.75),(13, gain),(14, gain*.75))
-        if gain > 0:
-            await ctx.send(f'Treble set to {(gain-1)*100}%')
-        elif gain < 0:
-            await ctx.send(f'Treble set to {(1-gain)*100}%')
+        death = [(10, gain*.75),(11, gain*.75),(12, gain*.75),(13, gain),(14, gain*.75)]
+        player.set_gains(death)
+        if gain:
+            await ctx.send(f'Treble set to {gain*100}%')
         else:
             await ctx.send('Treble set to 100%')
 
