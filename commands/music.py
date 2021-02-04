53344eeb330b24f68d2245a36ced7ae3f2666797
redmoogle <dakotamew@gmail.com>
2021-02-03 21:16:21 -0500

    Update music.py

--- commands/music.py
+++ commands/music.py
@@ -337,7 +337,7 @@ class Music(commands.Cog):
         gain = max(min(1, gain), -0.25)
         await player.set_gains((0, gain*.75),(1, gain*.75),(2, gain*.75),(3, gain),(4, gain*.75))
         if gain:
-            await ctx.send(f'Bass set to {gain*100}%')
+            await ctx.send(f'Bass set to {(gain+1)*100}%')
         else:
             await ctx.send('Bass set to 100%')
 
@@ -356,7 +356,7 @@ class Music(commands.Cog):
         gain = max(min(1, gain), -0.25)
         await player.set_gains((5, gain*.75),(6, gain*.75),(7, gain*.75),(8, gain),(9, gain*.75))
         if gain:
-            await ctx.send(f'Mids set to {gain*100}%')
+            await ctx.send(f'Mids set to {(gain+1)*100}%')
         else:
             await ctx.send('Mids set to 100%')
 
@@ -375,7 +375,7 @@ class Music(commands.Cog):
         gain = max(-0.25, min(1, gain))
         await player.set_gains((10, gain*.75),(11, gain*.75),(12, gain*.75),(13, gain),(14, gain*.75))
         if gain:
-            await ctx.send(f'Treble set to {gain*100}%')
+            await ctx.send(f'Treble set to {(gain+1)*100}%')
         else:
             await ctx.send('Treble set to 100%')
 
