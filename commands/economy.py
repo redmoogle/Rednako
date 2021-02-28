0b8a8d549b175056b1f82085c943ccfb4a08edce
redmoogle <dakotamew@gmail.com>
2021-02-28 10:47:42 -0500

    Implement basic economy

--- commands/economy.py
+++ commands/economy.py
@@ -5,6 +5,7 @@ Handles various money things
 
 # Standard Python Modules
 import random
+import asyncio
 
 # Discord Modules
 from discord.ext import commands
@@ -13,6 +14,37 @@ from discord.ext import commands
 from modules import jsonreader
 from modules import helpers
 
+def parse_duration(duration: int):
+    """
+    Parse duration into DD:HH:MM:SS
+    """
+    minutes, seconds = divmod(duration, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    duration = []
+    _ = "" # used for zfill
+    if days > 0:
+        duration.append(f'{round(days)}:')
+    if hours > 0:
+        _ = str(round(hours))
+        if days > 0:
+            _ = _.zfill(2)
+        _ += ":"
+        duration.append(_)
+    if minutes > 0:
+        _ = str(round(minutes))
+        if hours > 0:
+            _ = _.zfill(2)
+        _ += ":"
+        duration.append(_)
+    if seconds > 0:
+        _ = str(round(seconds))
+        if minutes > 0:
+            _ = _.zfill(2)
+        duration.append(_)
+
+    return ''.join(duration)
+
 class Economy(commands.Cog):
     """
     Economy Cog
@@ -20,10 +52,15 @@ class Economy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def cog_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            await ctx.send(f'Hold on there fellow heister, the cops are still hot on you wait some time({parse_duration(int(error.retry_after))})')
+
     @commands.command(
         name='payday',
         brief='steal money'
     )
+    @commands.cooldown(1, 21600, commands.BucketType.member)
     async def payday(self, ctx):
         """
         ''Steals money''
@@ -33,12 +70,16 @@ class Economy(commands.Cog):
             money = guild_economy[str(ctx.author.id)]
         except:
             jsonreader.write_file(ctx.guild.id, 'economy', {str(ctx.author.id): 0})
-            return await ctx.send('Lets set you up a bank account!')
+            await ctx.send('Lets set you up a bank account!')
+            await asyncio.sleep(5)
+            await ctx.send('Done! You now have a OffSafe:tm: Account!')
+            await asyncio.sleep(2)
 
         heisted = random.randint(5, 30)
-        money[str(ctx.author.id)] += heisted
-        jsonreader.write_file(ctx.guild.id, 'economy', money)
-        return await ctx.send(f'You just stole {heisted}!')
+        money += heisted
+        guild_economy[str(ctx.author.id)] = money
+        jsonreader.write_file(ctx.guild.id, 'economy', guild_economy)
+        return await ctx.send(f'You just stole ${heisted}!')
 
     @commands.command(
         name='check',
@@ -55,7 +96,7 @@ class Economy(commands.Cog):
             return await ctx.send('You\'re not registered with OffSafe:tm:!')
 
         info = [
-            'Money: ', f'{money}'
+            ['Total Balance: ', f'${money}']
         ]
         embed=helpers.embed(title=f'{ctx.author.name}\'s offshore account', image=ctx.author.avatar_url, fields=info)
         return await ctx.send(embed=embed)
