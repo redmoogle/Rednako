2278adf51a4f1f7e9b05fb4d91b4c72c4f376749
redmoogle <dakotamew@gmail.com>
2021-03-04 21:24:12 -0500

    songs now shows mins and seconds regardless

--- commands/music.py
+++ commands/music.py
@@ -69,17 +69,15 @@ def parse_duration(duration: int):
             _ = _.zfill(2)
         _ += ":"
         duration.append(_)
+    _ = str(round(minutes))
+    if hours > 0:
+        _ = _.zfill(2)
+    _ += ":"
+    duration.append(_)
+    _ = str(round(seconds))
     if minutes > 0:
-        _ = str(round(minutes))
-        if hours > 0:
-            _ = _.zfill(2)
-        _ += ":"
-        duration.append(_)
-    if seconds > 0:
-        _ = str(round(seconds))
-        if minutes > 0:
-            _ = _.zfill(2)
-        duration.append(_)
+        _ = _.zfill(2)
+    duration.append(_)
 
     return ''.join(duration)
 
