dd65aa7bac69ffd2339d396140eb204146adea8e
Bot PC <Bot PC>
2022-01-29 07:26:49 -0600

    fix

--- modules/__init__.py
+++ modules/__init__.py
@@ -5,4 +5,4 @@ Combines all the modules into one big import
 from .helpers import *
 from .manager import dash as manager
 from .animal import Animals
-from .redwave import *
+from .redlink import *
